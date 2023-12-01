bbc00b3b6b0a0e25e73abb574cb8bd57bb0b0381
open-meadow <rohan.varughese@protonmail.com>
2023-12-01 14:04:51 -0500

    Setup node, express and typescript

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,9 +1,7 @@
 import express from "express";
-
 const app = express();
-
 // Middleware
 app.use(express.json());
-
 // Connections and Listeners
 app.listen(5000, () => console.log('Server Open'));
+//# sourceMappingURL=index.js.map
