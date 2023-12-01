be5fb37ca1f312107f03c3a03e3a402f2aedb8b4
open-meadow <rohan.varughese@protonmail.com>
2023-12-01 15:41:04 -0500

    Create userSchema and chatSchema in models

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,8 +1,13 @@
 import express from "express";
 import { config } from 'dotenv';
+import morgan from 'morgan';
+import appRouter from "./routes/index.js";
 config();
 const app = express();
 // Middleware
 app.use(express.json());
+// remove in production
+app.use(morgan("dev"));
+app.use("api/v1", appRouter);
 export default app;
 //# sourceMappingURL=app.js.map
