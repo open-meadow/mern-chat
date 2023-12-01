c19aa0d6ef6fd2abfd87f31e08d2f71097bc359b
open-meadow <rohan.varughese@protonmail.com>
2023-12-01 16:49:11 -0500

    Successfully connect to /user endpoint from Postman

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,4 +1,13 @@
-export const getAllUsers = () => {
-    // get all users from database
+import User from "../models/User.js";
+export const getAllUsers = async (req, res, next) => {
+    try {
+        // get all users from database
+        const users = await User.find();
+        return res.status(200).json({ message: "OK", users });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
 };
 //# sourceMappingURL=user-controllers.js.map
