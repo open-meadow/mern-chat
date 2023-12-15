0b972a2c101c55446ad4bc8970a7391a79d64a26
open-meadow <rohan.varughese@protonmail.com>
2023-12-15 15:58:50 -0500

    Design app header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,16 @@
-import { AppBar } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
 import React from "react";
+import Logo from "./shared/Logo";
 
 const Header = () => {
-    return <AppBar></AppBar>;
-}
+  return (
+    <AppBar sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}>
+      <Toolbar sx={{ display: "flex" }}>
+        <Logo/>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Header;
